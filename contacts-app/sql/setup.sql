08a6412d5a897822f33831f7f53a50803634c8d8
Juan_Cigales <cigales.juan.mrm@gmail.com>
2024-07-09 11:56:11 +0200

    mensajes flash y ultimo commit

--- contacts-app/sql/setup.sql
+++ contacts-app/sql/setup.sql
@@ -2,19 +2,19 @@ DROP DATABASE IF EXISTS contacts_app;
 
 CREATE DATABASE contacts_app;
 
-CREATE TABLE users(
+USE contacts_app;
+
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     email VARCHAR(255) UNIQUE,
     password VARCHAR(255)
 );
 
-USE contacts_app;
-
 CREATE TABLE contacts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
-    phone_number VARCHAR(255)
+    user_id INT NOT NULL,
+    phone_number VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
-
-INSERT INTO contacts (name, phone_number) VALUES ("Pepe", "123456789");
